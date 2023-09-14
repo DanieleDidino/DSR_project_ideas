2a012a4cfae18afd95b2fdb3b433c62ff5ead3f4
MaCoZu <marcoz@posteo.de>
2023-09-14 10:35:13 +0200

    bot clone for experimentation

---
+++ marco_bot_clone/search_bot.py
@@ -0,0 +1,42 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.callbacks import StreamlitCallbackHandler
+from web_retriever import DocumentRetrievalSystem
+
+
+with st.sidebar:
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="langchain_search_api_key_openai", type="password"
+    )
+
+load_dotenv()
+openai_api_key= GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {
+            "role": "assistant",
+            "content": "Hi, I'm a chatbot who can search the web. How can I help you?",
+        }
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    doc_retriever = DocumentRetrievalSystem()
+
+    with st.chat_message("assistant"):
+        # st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        # response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+        answer, sources = doc_retriever.get_answer(st.session_state.messages)
+        st.session_state.messages.append({"role": "assistant", "content": answer})
+        st.write(answer, sources)
+
+
